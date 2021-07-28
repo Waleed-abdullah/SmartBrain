8dfcadc52f506d12171445782596f940d2479077
Waleed-Abdullah <w26waleed@gmail.com>
2021-07-28 17:44:37 +0500

    used fragmentation where i could and added some methods

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Rank = () => {
   return (
-    <div>
+    <>
       <div className="white f3">{'Waleed, your current rank is...'}</div>
       <div className="white f1">{'#2'}</div>
-    </div>
+    </>
   );
 };
 
