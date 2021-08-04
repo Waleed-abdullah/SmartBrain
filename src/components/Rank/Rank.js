a8a7f59467e2d1a452673059ee8b3ea63284b4e0
Waleed-Abdullah <w26waleed@gmail.com>
2021-08-04 19:41:28 +0500

    small design fix

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Rank = ({ name, entries }) => {
   return (
     <>
-      <div className="white f3">{`${name}, your current entry count is...${entries}`}</div>
-      <div className="white f1">{'#2'}</div>
+      <div className="white f3">{`${name}, your current entry count is...`}</div>
+      <div className="white f1">{entries}</div>
     </>
   );
 };
