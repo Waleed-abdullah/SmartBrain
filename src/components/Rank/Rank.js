44b4cf57124f5e4294fc59d3adf8f0d6d0d9de57
Waleed-Abdullah <w26waleed@gmail.com>
2023-08-20 13:10:58 +0500

    added lambda function rank api call

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 
 const Rank = ({ name, entries }) => {
+  const [emoji, setEmoji] = useState('🚫');
+
+  async function generateEmoji(entries) {
+    try {
+      const response = await fetch(`your lambda API URL here`);
+      const data = await response.json();
+      setEmoji(data.input);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  useEffect(() => {
+    generateEmoji(entries);
+  });
   return (
     <>
       <div className="white f3">{`${
         name.charAt(0).toUpperCase() + name.slice(1)
       }, your current entry count is...`}</div>
       <div className="white f1">{entries}</div>
+      <div className="white f3">{`Rank Badge: ${emoji}`}</div>
     </>
   );
 };
